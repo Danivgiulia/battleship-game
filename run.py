7bf4bad7403f0a7a70fe72b7ad221d28026d02c3
Danivgiulia <danielavgiulianelli@gmail.com>
2025-04-20 11:46:27 +0100

    add function for user input row and column number

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
-# Legend 
+# Legend
 
-# The 'X' mark will be used to mark a shot 
+# The 'X' mark will be used to mark a shot
 # The ' ' (the empty space) will signify that nothing has been done
-# The 'O' wil signify that the shot has been missed 
+# The 'O' wil signify that the shot has been missed
 
 # This will be used to generate random ship coorddinates
 from random import randint
 
-# The hidden board is for the computer to place the randomly generated coordinates
+# The hidden board is for the computer to place the randomly generated
+# coordinates
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 
-# The guess board is for the player to guess and keep track of where to put the shots 
+# The guess board is for the player to guess and keep track of where to put
+# the shots
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 # Because python is a language that starts the count from 0 instead
@@ -42,7 +44,24 @@ def generate_ships():
 # function for getting the ship location
 # bug for if no input then crash
 def get_coordinates():
-    pass
+    print()
+    
+    # Get valid row input
+    while True:
+        row = input('Please enter a ship row (1-8): ')
+        if row in '12345678':
+            break
+        print('Please enter a valid row.')
+
+    # Get valid column input
+    while True:
+        column = input('Please enter a ship column (A-H): ').upper()
+        if column in 'ABCDEFGH':
+            break
+        print('Please enter a valid column.')
+
+    return int(row) - 1, letters_to_numbers[column]
+
 
 # function that counts the total hits
 def total_hits():
